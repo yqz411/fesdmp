31add8c03846a2d112970952d0aaa7b83dd47c93
zhangzhaoyu <zhangzhaoyu0524@163.com>
2014-07-08 13:44:49 +0800

    add user

---
+++ src/main/java/cn/bjfu/fesdmp/domain/sys/User.java
@@ -0,0 +1,32 @@
+/** 
+ * Project Name:fesdmp 
+ * File Name:User.java 
+ * Package Name:cn.bjfu.fesdmp.domain.sys 
+ * Date:2014年7月8日 下午1:21:36 
+ * Copyright (c) 2014, zhangzhaoyu0524@163.com All Rights Reserved. 
+ * 
+*/  
+  
+package cn.bjfu.fesdmp.domain.sys;  
+
+import java.io.Serializable;
+
+/** 
+ * ClassName:User <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2014年7月8日 下午1:21:36 <br/> 
+ * @author   zhangzhaoyu 
+ * @version   
+ * @since    JDK 1.7 
+ * @see       
+ */
+public class User implements Serializable {
+
+	/**
+	 * @since JDK 1.7
+	 */
+	private static final long serialVersionUID = 4240247701595830143L;
+
+}
+ 
