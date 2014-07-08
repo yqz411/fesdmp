557e08189e61aafc1a9c7185ccc4e9f4193a960e
zhangzhaoyu <zhangzhaoyu0524@163.com>
2014-07-08 13:44:17 +0800

    update

--- src/main/java/cn/bjfu/fesdmp/web/BaseController.java
+++ src/main/java/cn/bjfu/fesdmp/web/BaseController.java
@@ -8,6 +8,29 @@
 */  
   
 package cn.bjfu.fesdmp.web;  
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import cn.bjfu.fesdmp.constant.AppConstants;
+import cn.bjfu.fesdmp.domain.sys.User;
+
+
 /** 
  * ClassName:BaseController <br/> 
  * Function: TODO ADD FUNCTION. <br/> 
@@ -19,6 +42,106 @@ package cn.bjfu.fesdmp.web;
  * @see       
  */
 public class BaseController {
+	
+	public static String errorMsg = "";
+	
+	@Autowired
+	protected ResourceBundleMessageSource messageSource;
+	
+	protected ModelAndView ajaxDone(int statusCode, String message) {
+		ModelAndView mav = new ModelAndView("ajaxDone");
+		mav.addObject("statusCode", statusCode);
+		mav.addObject("message", message);
+		return mav;
+	}
 
+	protected ModelAndView ajaxDoneSuccess(String message) {
+		return ajaxDone(200, message);
+	}
+
+	protected ModelAndView ajaxDoneError(String message) {
+		return ajaxDone(300, message);
+	}
+	protected String getMessage(String code) {
+		return this.getMessage(code, new Object[] {});
+	}
+
+	protected String getMessage(String code, Object arg0) {
+		return getMessage(code, new Object[] { arg0 });
+	}
+
+	protected String getMessage(String code, Object arg0, Object arg1) {
+		return getMessage(code, new Object[] { arg0, arg1 });
+	}
+
+	protected String getMessage(String code, Object arg0, Object arg1,
+			Object arg2) {
+		return getMessage(code, new Object[] { arg0, arg1, arg2 });
+	}
+
+	protected String getMessage(String code, Object arg0, Object arg1,
+			Object arg2, Object arg3) {
+		return getMessage(code, new Object[] { arg0, arg1, arg2, arg3 });
+	}
+
+	protected String getMessage(String code, Object[] args) {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+		Locale locale = localeResolver.resolveLocale(request);
+
+		return messageSource.getMessage(code, args, locale);
+	}
+	
+	
+	
+	@InitBinder
+	protected void initBinder(HttpServletRequest request,
+			ServletRequestDataBinder binder) throws Exception {
+		
+		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		 dateFormat.setLenient(false);
+		 binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
+	/**
+	 * 
+	 * addSessionUser:<br />
+	 * 将登陆的用户放入 session 中
+	 *
+	 * @author zhangzhaoyu
+	 * @param request
+	 * @param user
+	 */
+	public void addSessionUser(HttpServletRequest request, User user) {
+		request.getSession().setAttribute(AppConstants.SESSION_USER, user);
+	}
+	
+	/**
+	 * 
+	 * removeSessionUser:<br />
+	 * 将登陆的用户从 session 取出来
+	 *
+	 * @author zhangzhaoyu
+	 * @param request
+	 */
+	public void removeSessionUser(HttpServletRequest request) {
+		User user = (User) request.getAttribute(AppConstants.SESSION_USER);
+		if (user != null) {
+			request.getSession().removeAttribute(AppConstants.SESSION_USER);
+		}
+	}
+	
+	/**
+	 * 
+	 * detroySession:<br />
+	 * 清除 Session
+	 *
+	 * @author zhangzhaoyu
+	 * @param session
+	 */
+	public void detroySession(HttpSession session) {
+		session.invalidate();
+	}
+	
 }
  
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,9 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Home</title>
 </head>
 <body>
@@ -10,5 +12,8 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<p>
+	currentTime ${timesss }
+</p>
 </body>
 </html>
