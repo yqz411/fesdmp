0d856192a4294532a950fabf2232594a080ca5bf
zhangzhaoyu <zhangzhaoyu0524@163.com>
2014-07-09 00:45:20 +0800

    update

---
+++ src/main/java/cn/bjfu/fesdmp/sys/dao/ISystemLogDao.java
@@ -0,0 +1,24 @@
+/** 
+ * Project Name:fesdmp 
+ * File Name:ISystemLogDao.java 
+ * Package Name:cn.bjfu.fesdmp.sys.dao 
+ * Date:2014年7月9日 上午12:37:30 
+ * Copyright (c) 2014, zhangzhaoyu0524@163.com All Rights Reserved. 
+ * 
+*/  
+  
+package cn.bjfu.fesdmp.sys.dao;  
+/** 
+ * ClassName:ISystemLogDao <br/> 
+ * Function: ISystemLogDao. <br/> 
+ * Reason:   ISystemLogDao. <br/> 
+ * Date:     2014年7月9日 上午12:37:30 <br/> 
+ * @author   zhangzhaoyu 
+ * @version   1.0
+ * @since    JDK 1.7 
+ * @see       
+ */
+public interface ISystemLogDao<T> extends IGenericDao<T> {
+
+}
+ 
---
+++ src/main/java/cn/bjfu/fesdmp/sys/dao/impl/SystemLogDaoImpl.java
@@ -0,0 +1,35 @@
+/** 
+ * Project Name:fesdmp 
+ * File Name:SystemLogDaoImpl.java 
+ * Package Name:cn.bjfu.fesdmp.sys.dao.impl 
+ * Date:2014年7月9日 上午12:38:25 
+ * Copyright (c) 2014, zhangzhaoyu0524@163.com All Rights Reserved. 
+ * 
+*/  
+  
+package cn.bjfu.fesdmp.sys.dao.impl;  
+
+import org.springframework.stereotype.Repository;
+
+import cn.bjfu.fesdmp.domain.sys.SystemLog;
+import cn.bjfu.fesdmp.sys.dao.ISystemLogDao;
+
+/** 
+ * ClassName:SystemLogDaoImpl <br/> 
+ * Function: SystemLogDaoImpl. <br/> 
+ * Reason:   SystemLogDaoImpl. <br/> 
+ * Date:     2014年7月9日 上午12:38:25 <br/> 
+ * @author   zhangzhaoyu 
+ * @version   
+ * @since    JDK 1.7 
+ * @see       
+ */
+@Repository
+public class SystemLogDaoImpl extends AbstractGenericDao<SystemLog> implements ISystemLogDao<SystemLog> {
+
+	public SystemLogDaoImpl() {
+		super(SystemLog.class);
+	}
+
+}
+ 
